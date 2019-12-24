4cd0b7298f8a1fef7315b0f50326c9e4a99ded51
Jhonatan Restrepo <jhonatanrg@live.com>
2019-12-24 22:49:36 +0000

    Navigation bar succesfully finished for web pages

---
+++ frontend/src/components/assets/Navigation.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import BrandLogo from "../images/80.png";
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div className="navigation">
+        <nav className="navbar navbar-expand-lg text-dark bg-white">
+          <Link to="/" className="navbar-logo text-white">
+            <img src={BrandLogo} alt="" className="" />{" "}
+            <h4 className="navbar-brand ml-4"> Aurora Development</h4>
+          </Link>
+          <div className="container">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="true"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto ml-3">
+                <li className="nav-item active">
+                  <Link className="nav-link text-white" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/software">
+                    Software
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/library">
+                    Library
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/our-services">
+                    Our Services
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/contact-us">
+                    Contact us
+                  </Link>
+                </li>
+              </ul>
+              <div className="form-inline my-2 my-lg-0">
+                <button className="btn btn-outline-dark mr-sm-2">Signup</button>
+                <button className="btn btn-outline-dark my-2 my-sm-0">
+                  Signin
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <hr className="nav-hr mt-0" />
+      </div>
+    );
+  }
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./css/bootstrap.min.css";
+import "./css/mdb-boots/css/mdb.min.css";
+import "./css/style.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
